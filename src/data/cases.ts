ac03663053f24604e30bf177ae3451c5d325b8e9
sebadog <sebadog@gmail.com>
2025-05-24 23:34:06 -0400

    Create cases data file and fix import

---
+++ src/data/cases.ts
@@ -0,0 +1,14 @@
+// Default cases data
+export const cases = [
+  {
+    id: '1',
+    title: 'Sample Case',
+    accessionNumber: 'ACC001',
+    clinicalInfo: 'This is a sample case for testing purposes.',
+    expectedFindings: ['Sample finding 1', 'Sample finding 2'],
+    additionalFindings: [],
+    summaryOfPathology: 'This is a sample summary of pathology.',
+    images: [],
+    completed: false
+  }
+];
