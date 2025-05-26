e4ab2818b70c521e9e2a90d1235096c7b7749cef
sebadog <sebadog@gmail.com>
2025-05-26 08:59:36 -0400

    Update CaseViewer component

--- src/components/CaseViewer.tsx
+++ src/components/CaseViewer.tsx
@@ -409,7 +409,7 @@ function CaseViewer() {
 
         {showTeachingPoints && (
           <div className="mb-6">
-            <h3 className="font-medium text-gray-700 mb-2">Teaching Points:</h3>
+            <h3 className="font-medium text-gray-700 mb-2">Correct Impression:</h3>
             <div className="bg-green-50 border-l-4 border-green-500 p-4 rounded">
               <h4 className="font-medium mb-2">Summary of Pathology:</h4>
               <p>{currentCase.summary_of_pathology}</p>
